dff13bd5dd40228dfa4e0d5338a3fec7f3739c45
josué <josuebeurensilvaa@gmail.com>
2025-03-26 06:40:17 -0300

    criando hero  fonte

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin'); // Corrigido de 'gulp-imagemim'
 
 // Tarefa para compilar os arquivos SCSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass({ outputStyle: 'compressed' })) // Usa sass para SCSS
         .pipe(gulp.dest('./dist/css'));
+}
+
+// Tarefa para otimizar imagens
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin()) // Usa imagemin para imagens
+        .pipe(gulp.dest('./dist/images'));
 }
 
 // Tarefa de monitoramento
 function watchFiles() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Passa a função 'styles' diretamente
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Monitora SCSS
+    gulp.watch('./src/images/**/*', gulp.parallel(images));   // Monitora imagens
 }
 
 // Exporta as tarefas
-exports.default = styles; // Tarefa padrão
+exports.default = gulp.parallel(styles, images); // Executa styles e images em paralelo
 exports.watch = watchFiles; // Tarefa 'watch'
